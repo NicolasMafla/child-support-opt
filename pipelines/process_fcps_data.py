cc364f039ee55a52de7fabc1eb270ae4652b38bb
NicolasMafla <nicolas.mafla.checa@gmail.com>
2025-03-12 15:44:02 -0500

    Process fcp data pipeline created

---
+++ pipelines/process_fcps_data.py
@@ -0,0 +1,10 @@
+from src.data import ExcelLoader, FCPDataProcessor
+from src.utils import save_pandas_data
+
+loader = ExcelLoader()
+raw_data_path = "./data/raw/fcps.xlsx"
+processed_data_path = "./data/processed/processed_fcps.xlsx"
+
+processor = FCPDataProcessor(loader=loader)
+processed_data = processor.process_data(filepath=raw_data_path)
+save_pandas_data(input_data=processed_data, filepath=processed_data_path)
