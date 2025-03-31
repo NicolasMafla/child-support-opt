e7c77b2548422ca26a6902946e0e2e56caebf7d9
NicolasMafla <nicolas.mafla.checa@gmail.com>
2025-03-31 14:33:10 -0500

    Data processing pipelines updated

---
+++ pipelines/consolidation_balance_data.py
@@ -0,0 +1,20 @@
+import os
+import pandas as pd
+from src.data import ExcelLoader
+from src.utils import save_pandas_data, list_files
+
+loader = ExcelLoader()
+source_path = "./data/processed/balance"
+final_path = "./data/processed/final"
+filepaths = list_files(path=source_path)
+filename = "balance"
+ext = ".xlsx"
+
+data_list = []
+for filepath in filepaths:
+    data = loader.load(filepath=filepath)
+    data_list.append(data)
+
+final = pd.concat(data_list)
+output_filepath = os.path.join(final_path, f"{filename}{ext}")
+save_pandas_data(input_data=final, filepath=output_filepath)
--- pipelines/process_fcps_data.py
+++ pipelines/process_fcps_data.py
@@ -1,10 +1,14 @@
+import os
 from src.data import ExcelLoader, FCPDataProcessor
 from src.utils import save_pandas_data
 
 loader = ExcelLoader()
 raw_data_path = "./data/test/fcps.xlsx"
-processed_data_path = "./data/processed/processed_fcps.xlsx"
+final_path = "./data/processed/final"
+filename = "fcps"
+ext = ".xlsx"
 
 processor = FCPDataProcessor(loader=loader)
 processed_data = processor.process(filepath=raw_data_path)
-save_pandas_data(input_data=processed_data, filepath=processed_data_path)
+output_filepath = os.path.join(final_path, f"{filename}{ext}")
+save_pandas_data(input_data=processed_data, filepath=output_filepath)
