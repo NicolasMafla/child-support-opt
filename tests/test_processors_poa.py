838e4bb2639125257f45331d29cd97cee3417fe6
NicolasMafla <nicolas.mafla.checa@gmail.com>
2025-03-31 12:11:35 -0500

    Processors tests refactored

--- tests/test_processors_poa.py
+++
@@ -1,7 +0,0 @@
-from src.data import CSVLoader, POADataProcessor
-
-def test_fcp_process_data():
-    loader = CSVLoader()
-    processor = POADataProcessor(loader=loader)
-    processed_data = processor.process(filepath="./data/test/EC0107.csv")
-    assert len(processed_data) > 0
