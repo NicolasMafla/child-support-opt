bd1d3260c72cdd6ea384cf74ccd74905fed194a0
NicolasMafla <nicolas.mafla.checa@gmail.com>
2025-04-14 17:05:04 -0500

    LP problem formulation updated

--- src/modelling/optimizer.py
+++ src/modelling/optimizer.py
@@ -43,12 +43,13 @@ class BudgetLpOptimizer(LpOptimizer):
         self.model.setObjective(obj_func)
 
     def add_constraints(self) -> None:
-        c1 = self.params["C1"] * self.variables["x1"] + self.params["C2"] * self.variables["x2"] == self.params["B"]
+        c1 = self.params["C1"] * self.variables["x1"] + self.params["C2"] * self.variables["x2"] <= self.params["B"] * (1 - 0.05)
         c2 = self.variables["x1"] + self.variables["x2"] <= self.params["N_max"]
-        c3 = self.variables["x1"] -1 >= 0
-        c4 = self.variables["x2"] -1 >= 0
+        c3 = self.variables["x1"] >= 1
+        c4 = self.variables["x2"] >= 1
+        c5 = self.variables["x1"] >= 0.1 * (self.variables["x1"] + self.variables["x2"])
 
-        for const in [c1, c2, c3, c4]:
+        for const in [c1, c2, c3, c4, c5]:
             self.model.addConstraint(constraint=const)
 
     def solve(self) -> None:
@@ -58,12 +59,10 @@ class BudgetLpOptimizer(LpOptimizer):
 pulp.LpSolverDefault.msg = False
 
 params = {
-    "C1" : 10,  # Costo por niño tipo 1 (0 a 12 años)
-    "C2" : 12,  # Costo por niño tipo 2 (13 a 18 años)
-    "I1" : 15,  # Ingreso por niño tipo 1
-    "I2" : 18,  # Ingreso por niño tipo 2
-    "B" : 1000,  # Presupuesto asignado
-    "N_max" : 100  # Número máximo de niños
+    "C1" : 5787.51 / 275 + 5,  # Costo por niño tipo 1 (0 a 5 años)
+    "C2" : 5787.51 / 275,  # Costo por niño tipo 2 (6 a 22 años)
+    "B" : 6000,  # Presupuesto asignado
+    "N_max" : 1000  # Número máximo de niños
 }
 
 model = pulp.LpProblem(name="compassion", sense=pulp.LpMaximize)
@@ -80,11 +79,12 @@ b.solve()
 
 if pulp.LpStatus[b.model.status] == "Optimal":
     print(f"Estado de la solución: {pulp.LpStatus[model.status]}")
-    print(f"El número mínimo de niños tipo 1 necesarios es: {pulp.value(variables['x1'])}")
-    print(f"El número mínimo de niños tipo 2 necesarios es: {pulp.value(variables['x2'])}")
+    print(f"n1: {pulp.value(variables['x1'])}")
+    print(f"n2: {pulp.value(variables['x2'])}")
+    print(f"n1 + n2: {pulp.value(variables['x1']) + pulp.value(variables['x2'])}")
 
     # Imprimir el valor de la restricción budget_constraint (costo total)
     total_cost = params["C1"] * pulp.value(variables["x1"]) + params["C2"] * pulp.value(variables["x2"])
-    print(f"El costo total asignado a los niños es: {total_cost}")
+    print(f"Presupuesto asignado: {params['B']} vs. costo optimizado: {total_cost}")
 else:
     print(f"Estado: {pulp.LpStatus[model.status]}")
